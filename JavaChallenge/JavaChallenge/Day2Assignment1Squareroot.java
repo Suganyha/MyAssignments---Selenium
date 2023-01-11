fcacdc214ab7b310505a104bc1c9713bff404b8b
Suganyha <74657490+Suganyha@users.noreply.github.com>
2023-01-11 22:29:32 +0530

    Add files via upload
    Adding the day 2 and day 3 assignments of Java Challenge

---
+++ JavaChallenge/JavaChallenge/Day2Assignment1Squareroot.java
@@ -0,0 +1,12 @@
+package JavaChallenge;
+
+public class Day2Assignment1Squareroot {
+
+	public static void main(String[] args) {
+		int X=4;
+		int sqrt = (int) Math.sqrt(X);
+		System.out.println(sqrt);
+
+	}
+
+}
---
+++ JavaChallenge/JavaChallenge/Day2Assignment2.java
@@ -0,0 +1,12 @@
+package JavaChallenge;
+
+public class Day2Assignment2 {
+
+	public static void main(String[] args) {
+		int x=8;
+		int sqrt = (int) Math.sqrt(x);
+		System.out.println(sqrt);
+
+	}
+
+}
